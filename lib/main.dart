b504770a8e2f1273ea1d7524645d534be47e47e1
NEOLYH <895928691@qq.com>
2019-09-17 23:23:26 +0800

    112

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.yellow,
       ),
       home: HomePage(title: 'NEOFlutter'),
     );
